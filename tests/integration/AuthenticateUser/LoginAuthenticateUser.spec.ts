5d9b03503dd5c4f4190d100d528b793c9d78b9ae
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-10-28 11:39:03 -0300

    implementação dos testes de integração do endpoints para logar no sistema

---
+++ tests/integration/AuthenticateUser/LoginAuthenticateUser.spec.ts
@@ -0,0 +1,139 @@
+import { app } from "../../../src/app";
+import { UserCreate } from "../../../src/entities/User";
+
+import { describe, test, expect, beforeAll, beforeEach, afterAll } from "@jest/globals";
+import request from "supertest";
+import { hash } from 'bcrypt'
+
+import { PrismaClient } from "@prisma/client";
+import { after } from "node:test";
+import exp from "node:constants";
+
+
+
+const prisma = new PrismaClient();
+
+const userCreateTests: UserCreate = {
+    name: "Testes",
+    email: "testes@testes.com",
+    password: "1234567890",
+}
+
+describe("Autenticação de Usuário - Integração", () => {
+    beforeAll(async () => {
+        await prisma.user.deleteMany();
+        const passwordHash = await hash(userCreateTests.password, 8)
+        const userCreate = await prisma.user.create({
+
+            data: {
+                name: userCreateTests.name,
+                email: userCreateTests.email,
+                password: passwordHash,
+            }
+        });
+    });
+
+    afterAll(async () => {
+        await prisma.user.deleteMany();
+        await prisma.$disconnect();
+    });
+
+    test("Deve ser capaz de autenticar um usuário", async () => {
+
+        const response = await request(app).post("/authenticate/login").send({
+            email: userCreateTests.email,
+            password: userCreateTests.password,
+        });
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                token: expect.any(String),
+                user: expect.objectContaining({
+                    email: expect.any(String), id: expect.any(String)
+                })
+            })
+        );
+    });
+
+    test("Não deve ser capaz de autenticar um usuário com senha incorreta", async () => {
+        const response = await request(app).post("/authenticate/login").send({
+            email: userCreateTests.email,
+            password: "Senha incorreta",
+        });
+        expect(response.status).toBe(404);
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                message: expect.any(String),
+                statusCode: expect.any(Number),
+            })
+        );
+    });
+
+    test("Não deve ser capaz de autenticar um usuário com o email incorreto", async () => {
+        const response = await request(app).post("/authenticate/login").send({
+            email: "emailIncorreto@teste.com",
+            password: userCreateTests.password,
+        });
+        expect(response.status).toBe(404);
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                message: expect.any(String),
+                statusCode: expect.any(Number),
+            })
+        );
+    });
+
+    test("Não deve ser capaz de autenticar com o corpo da requição vazia", async () => {
+        const response = await request(app).post("/authenticate/login").send({
+            email: "",
+            password: "",
+        });
+        expect(response.status).toBe(400);
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                error: expect.arrayContaining([
+                    expect.objectContaining({
+                        message: expect.any(String),
+                        path: expect.any(String),
+                    })
+                ])
+            })
+        );
+    });
+
+    test("Não deve ser capaz de autenticar sem informar o campo 'email'", async () => {
+        const response = await request(app).post("/authenticate/login").send({
+            email: "",
+            password: userCreateTests.password,
+        });
+        expect(response.status).toBe(400);
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                error: expect.arrayContaining([
+                    expect.objectContaining({
+                        message: expect.any(String),
+                        path: expect.any(String),
+                    })
+                ])
+            })
+        );
+    });
+
+    test("Não deve ser capaz de autenticar sem informar o campo 'password'", async () => {
+        const response = await request(app).post("/authenticate/login").send({
+            email: userCreateTests.email,
+            password: "",
+        });
+        expect(response.status).toBe(400);
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                error: expect.arrayContaining([
+                    expect.objectContaining({
+                        message: expect.any(String),
+                        path: expect.any(String),
+                    })
+                ])
+            })
+        );
+    });
+});
